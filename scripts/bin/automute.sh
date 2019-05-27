8dd8417ab891a7600573def798d8908bd49adee8
Rui Marques <ruifmarques@pm.me>
2019-05-27 02:43:29 +0200

    Added tv script. Changed mpv max buffer size.

--- scripts/bin/automute.sh
+++ scripts/bin/automute.sh
@@ -7,9 +7,9 @@ do
     win="$(xdotool getwindowfocus getwindowpid)"
     if [ "$LAST_WIN_ACTIVE" != "$win" ]; then
         focus_index=$(pacmd list-sink-inputs | awk '/index:/{si = $2}; /'"$win"'/{print si; exit}')
-        [[ "$focus_index" ]] && pactl set-sink-input-mute "$focus_index" false
+        [ -n "$focus_index" ] && pactl set-sink-input-mute "$focus_index" false
         unfocus_index=$(pacmd list-sink-inputs | awk '/index:/{si = $2}; /'"$LAST_WIN_ACTIVE"'/{print si; exit}')
-        [[ "$focus_index" ]] && [[ "$unfocus_index" ]] && pactl set-sink-input-mute "$unfocus_index" true
+        [ -n "$focus_index" ] && [ -n "$unfocus_index" ] && pactl set-sink-input-mute "$unfocus_index" true
         LAST_WIN_ACTIVE="$win"
     fi
 done
