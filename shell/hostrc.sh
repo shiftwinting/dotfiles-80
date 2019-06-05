248f218fbd59421b916ae5eaed4969536757cc2d
Rui Marques <ruifmarques@pm.me>
2019-06-05 02:09:11 +0200

    added protonmail bridge to hostc

--- shell/hostrc.sh
+++ shell/hostrc.sh
@@ -3,8 +3,7 @@
 if [ "$(hostname)" = "garry" ]; then
     xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Tapping Enabled" 1
     xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Natural Scrolling Enabled" 1
-    # protonmail-bridge --no-window &
-    # :
+    protonmail-bridge --no-window &
 elif [ "$(hostname)" = "dev-004p" ]; then
     synclient HorizTwoFingerScroll=1
     synclient VertScrollDelta=-77
