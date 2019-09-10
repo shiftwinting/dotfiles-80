2513f5718d424833a123c15ef750a6a048117216
Rui Marques <ruifmarques@pm.me>
2019-09-11 01:37:59 +0200

    Make ctrl and shift behave as esc when tapped and caps lock to behave as ctrl when held.

--- X/bin/kb-setup.sh
+++ X/bin/kb-setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 setxkbmap -layout es
-setxkbmap -option caps:swapescape
+setxkbmap -option "caps:ctrl_modifier"
+xcape -e "Caps_Lock=Escape;Control_L=Escape;Shift_L=Escape"
 
 xkb_id=$(xinput --list --id-only "SONiX USB DEVICE" 2> /dev/null) || exit 0
 
