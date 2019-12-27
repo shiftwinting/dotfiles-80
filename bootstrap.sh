3c596580d2f6ca075666e13e1a4207a31ff6f3c8
Rui Marques <ruifmarques@pm.me>
2019-12-27 18:52:42 +0100

    feat(bluetooth): add support for better codecs and mpris

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,7 @@ mkdir -p ~/.ssh/sockets
 cp -r ~/dotfiles/systemd ~/.config
 systemctl --user enable --now tmux@ssh
 systemctl --user enable --now tmux@tty
+systemctl --user enable --now mpris-proxy
 
 if [ ! -d 'st' ]; then
     git clone https://github.com/ruifm/st.git
