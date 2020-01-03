b9f608673f248dd6ce5c2eaceb9af0b5043961dd
Rui Marques <ruifmarques@pm.me>
2020-01-03 13:41:57 +0100

    fix(sh): set PAGER when page is installed, minor fixes

--- sh/.env.sh
+++ sh/.env.sh
@@ -11,7 +11,7 @@ else
     if ! [ -x "/usr/bin/fzf" ] && ! [ -x "/usr/local/bin/fzf" ]; then
         PATH="$HOME"/.fzf/bin:"$PATH"
     fi
-    if [[ "$(hostname)" = "liselle" ]]; then
+    if [ "$(hostname)" = "liselle" ]; then
         PATH=/usr/local/cuda/bin:"$PATH"
     fi
     export PATH
@@ -20,7 +20,7 @@ else
     export MANPATH
 
     LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
-    if [[ "$(hostname)" = "liselle" ]]; then
+    if [ "$(hostname)" = "liselle" ]; then
         LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
     fi
     export LD_LIBRARY_PATH
@@ -34,10 +34,20 @@ export GPG_TTY
 export GPG_AGENT_INFO=1
 export ENV="$HOME"/.shinit
 export EDITOR='nvim'
+if command -v "nvim" > /dev/null 2>&1; then
+    EDITOR='nvim'
+elif command -v "vim" > /dev/null 2>&1; then
+    EDITOR='vim'
+else
+    EDITOR='vi'
+fi
+if command -v "page" > /dev/null 2>&1; then
+    export PAGER="page -q 90000"
+    export MANPAGER="page -C -e 'au User PageDisconnect sleep 100m|%y p|enew! |bd! #|pu p|set ft=man'"
+fi
 export LANG=en_US.UTF-8
 export ARCHFLAGS="-arch x86_64"
 export OPENER='xdg-open'
-export FZF_BASE=$HOME/.fzf/bin/fzf
 export FZF_DEFAULT_COMMAND="rg --files --hidden -g '!.git/*'"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS="--layout=reverse --inline-info --cycle --color=dark --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7"
