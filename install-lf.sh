cd4b6e8817228ecc4a9028b1e5805ed9fbf6108f
Rui Marques <ruifmarques@pm.me>
2020-04-14 11:41:48 +0200

    fix lf install script

--- install-lf.sh
+++ install-lf.sh
@@ -7,7 +7,7 @@ current_wd="$(pwd)"
 cd /tmp
 
 wget "https://github.com/gokcehan/lf/releases/download/r14/lf-linux-amd64.tar.gz"
-tar xf lf*
+tar xf lf-linux-amd64.tar.gz
 cp lf "$HOME"/.local/bin
 
 cd "$current_wd"
