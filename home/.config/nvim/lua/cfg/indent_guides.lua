22acf46c0cd80924bc42ca47e8c7907b5d8105c1
Rui Marques <mail@ruimarques.xyz>
2021-02-26 18:18:08 +0100

    nothin to see

--- home/.config/nvim/lua/cfg/indent_guides.lua
+++ home/.config/nvim/lua/cfg/indent_guides.lua
@@ -1,6 +1,6 @@
 require('indent_guides').setup{
     exclude_filetypes = {
         'help', 'man', 'terminal', 'packer', 'TelescopePrompt', 'fugitive',
-        'diff'
+        'diff', 'log'
     },
 }
