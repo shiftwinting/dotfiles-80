d4b52bb4caccb81ac9d8cb4947bc98842d6cdbf3
Rui Marques <mail@ruimarques.xyz>
2021-03-01 19:47:42 +0100

    tex updates

--- home/.config/nvim/lua/cfg/vimtex.lua
+++ home/.config/nvim/lua/cfg/vimtex.lua
@@ -1,20 +1,5 @@
 vim.g.tex_flavor = 'latex'
-vim.g.vimtex_compiler_progname = 'nvr'
 vim.g.vimtex_format_enabled = 1
-vim.g.vimtex_quickfix_autojump = 1
-vim.g.vimtex_quickfix_autoclose_after_keystrokes = 3
+vim.g.vimtex_syntax_conceal_default = 0
 vim.g.vimtex_view_method = 'zathura'
-vim.g.vimtex_complete_img_use_tail = 1
-vim.g.vimtex_compiler_latexmk = {callback = 0}
-
-local M = {}
-
-function M.getCompletionItems(prefix)
-    local items = vim.api.nvim_call_function('vimtex#complete#omnifunc',
-                                             {0, prefix})
-    return items
-end
-
-M.complete_item = {item = M.getCompletionItems}
-
-return M
+vim.g.vimtex_quickfix_open_on_warning = 0
